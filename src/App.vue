dc862e9f1350735a23eb6f4a87166f93e7add581
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2025-04-29 09:31:09 +0700

    Sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,49 @@
-<template>Hi</template>
+<template>
+  <div id="app">
+    <SideBar />
+    <div class="main-content">Hi</div>
+  </div>
+</template>
 
 <script>
+import SideBar from "./components/Navigation/SideBar.vue";
+
 export default {
   name: "App",
+  components: {
+    SideBar,
+  },
 };
 </script>
 
-<style></style>
+<style>
+body {
+  margin: 0px !important;
+  scrollbar-width: none;
+  font-family: "Inter", sans-serif;
+  overflow-y: hidden;
+}
+
+::-webkit-scrollbar {
+  display: none;
+}
+</style>
+
+<style scoped>
+#app {
+  display: flex;
+  min-height: 100vh;
+}
+
+.main-content {
+  flex-grow: 1;
+  background-color: #f6f5f8;
+}
+
+.content {
+  padding: 20px;
+  height: 100dvh;
+  overflow-y: scroll;
+  box-sizing: border-box;
+}
+</style>
---
+++ src/components/Navigation/SideBar.vue
@@ -0,0 +1,171 @@
+<template>
+  <div :class="['sidebar', { collapsed }]">
+    <div class="sidebar-title">
+      <h1 v-if="!collapsed">Counsellors</h1>
+      <i class="fas fa-bars" @click="toggleCollapse"></i>
+    </div>
+    <ul>
+      <li
+        v-for="(item, index) in menuItems"
+        :key="index"
+        :class="{
+          active: activeItem === item.name,
+          'logout-item': item.name === 'logout',
+        }"
+        @click="setActive(item.name)"
+      >
+        <i :class="item.icon"></i>
+        <span v-if="!collapsed">{{ item.label }}</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SideBar",
+  data() {
+    return {
+      activeItem: "home",
+      collapsed: false,
+      menuItems: [
+        { name: "home", label: "Home", icon: "far fa-file" },
+        { name: "themes", label: "Themes", icon: "far fa-image" },
+        { name: "expenses", label: "Expenses", icon: "far fa-credit-card" },
+        {
+          name: "clientInfo",
+          label: "Client Information",
+          icon: "far fa-address-book",
+        },
+        { name: "feedback", label: "Feedback", icon: "far fa-comments" },
+        { name: "comments", label: "Comments", icon: "far fa-comment-dots" },
+      ],
+    };
+  },
+  methods: {
+    setActive(item) {
+      this.activeItem = item;
+    },
+    toggleCollapse() {
+      this.collapsed = !this.collapsed;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.sidebar {
+  width: 260px;
+  background-color: rgb(255, 255, 255);
+  color: rgba(47, 43, 61, 0.9);
+  min-height: 100%;
+  max-height: 100vh;
+  padding-top: 20px;
+  position: relative;
+  z-index: 10;
+  font-size: 15px;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
+    rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  transition: width 0.3s ease;
+  overflow-y: scroll;
+  scrollbar-width: none;
+}
+
+.sidebar.collapsed {
+  width: 80px;
+  padding-top: 30px;
+}
+
+.sidebar-title {
+  padding: 0 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.sidebar-title h1 {
+  font-family: "Unica One", sans-serif;
+  font-weight: 400;
+  margin: 0;
+  color: #0b508b;
+}
+
+.sidebar-title i {
+  cursor: pointer;
+  font-size: 22px;
+}
+
+.sidebar ul {
+  list-style: none;
+  padding: 0;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+}
+
+.sidebar ul li {
+  padding: 15px 10px;
+  cursor: pointer;
+  border-radius: 5px;
+  margin: 4px 7px;
+  display: flex;
+  align-items: center;
+  transition: background-color 0.3s ease;
+  text-wrap: nowrap;
+}
+
+.sidebar ul li.logout-item {
+  margin-top: auto;
+  transition: color 0.3s ease;
+}
+
+.sidebar ul li:hover {
+  background-color: #f0f0f1;
+}
+
+.sidebar ul li.logout-item:hover i {
+  color: #ff4d4f;
+}
+
+.sidebar ul li.active {
+  background: linear-gradient(
+    270deg,
+    rgba(115, 103, 240, 0.7),
+    rgb(115, 103, 240)
+  );
+
+  color: white;
+}
+
+.sidebar ul li i {
+  margin-right: 10px;
+  margin-left: 5px;
+  color: rgba(47, 43, 61, 0.9);
+  font-size: 16px;
+}
+
+.sidebar ul li.active i {
+  color: white;
+}
+
+.sidebar.collapsed ul li,
+.sidebar.collapsed .sidebar-title {
+  justify-content: center;
+}
+
+.sidebar.collapsed ul li i {
+  margin: 0;
+  padding: 10px;
+  font-size: 18px;
+}
+
+@media (max-width: 699px) {
+  .sidebar,
+  .sidebar.collapsed {
+    display: none;
+  }
+}
+</style>
